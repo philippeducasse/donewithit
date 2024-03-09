525414285b09a0650dd0f9e6d00a7c45892da1a6
philo <ducassephi@hotmail.fr>
2024-03-09 11:17:38 +0100

    styled home screen

--- App.js
+++ App.js
@@ -1,20 +1,100 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  SafeAreaView,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+  TouchableHighlight,
+  TouchableNativeFeedback,
+  Button,
+  Alert,
+  Platform,
+  StatusBar,
+  Dimensions,
+  ImageBackground
+} from "react-native";
 
 export default function App() {
+  // make sure to remove these for production, otherwise leads to performance issues
+  console.log(Dimensions.get('screen'));
+  let x = 1;
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // these views all get converted into their native versions. they are just representations
+    // safeareaview only works on IOS
+    <SafeAreaView style={styles.container}>
+      <ImageBackground source={require("./assets/background.jpg")} resizeMode="cover" style={styles.image} />
+      <View style={styles.logoContainer}>
+        <Image source= {require('./assets/logo-red.png')} style= {styles.logo}/>
+        <Text>Sell yo stuff</Text>
+      </View>
+      <View style={styles.login}/>
+      <View style={styles.register}/>
+      {/* <Text numberOfLines={1} onPress={() => console.log("hello")}>
+        Open up App.js to start working on your app! lorem fdg gfsdfg sdfg dsfg
+        sdfg sdfg dsfg sdfg sdg dsg sd{" "}
+      </Text> */}
+      {/* require is for assets */}
+      {/* <Image source={require("./assets/splash.png")} />  */}
+      {/* for urls we use  this object with uri property and DEFINE DIMENSIONS*/}
+      {/* <TouchableNativeFeedback
+        onPress={() => {
+          console.log("image touched");
+        }}
+      > */}
+        {/* <Image
+          fadeDuration={1000}
+          width={200}
+          height={300}
+          source={{ uri: "https://picsum.photos/200/300" }}
+        /> */}
+        {/* <View
+          style={{ width: '100%', height: '30%', backgroundColor: "dodgerblue" }}
+        ></View>
+      </TouchableNativeFeedback>
+      <Button title="click me!" onPress={() => Alert.alert('Achtung','Button Tapped',[{text: 'Yes'}, {text: 'Nein'}])} />
+      <StatusBar style="auto" /> */}
+    </SafeAreaView>
   );
 }
 
+// this is not css, it is a representation that then gets compiled into the native styling through react native
+// StyleSheet.create is an api that validates our styling code
+// if you enter invalid stuff inline it doesnt through any errors
+// recommended to use stylsheet API
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
+  image: {
+    flex:10,
+    height: '100%',
+    width: '100%',
+  },
+  login: {
+    flex:1,
+    backgroundColor: 'dodgerblue',
+    width:'100%',
+  },
+  register: {
+    flex: 1,
+    backgroundColor: 'tomato',
+    width:'100%',
+  },
+  logoContainer:{
+    position: 'absolute',
+    top: 150,
+    left: '50%',
+    transform: [{ translateX: -50 }, { translateY: -50 }],
+  },
+  logo:{
+    width:100,
+    height: 100,
+    marginBottom: 20
+  }
 });
+
+//
---
+++ notes.md
@@ -0,0 +1,26 @@
+what are the types of data that control a component:
+
+PROPS & STATE
+
+PROPS are set by the parent and they are fixed throughout the lifetune of a component . for data that is going to change we use STATE
+
+## Gesture responder system
+manages the lifecycle of festures in the app. For example, a tapp can become a scroll
+
+## DIP
+density independent pixels
+what is used to determine the width of components across devices
+physical pixels = DIPS x Scale factor 
+eg: {"fontScale": 1, "height": 856.1777777777778, "scale": 2.8125, "width": 384}
+
+## Scroll View vs FLATList view:
+render list of similar items or array / data is very big: FLATLIST
+Render generic content in a sccrollable container and data is small: SCROLLVIEW
+
+## Flexbox
+
+In react native, flexbox aligns items to column by default
+POSITION is be default RELATIVE
+
+## Align-content:
+only works when wrapping is enabled
